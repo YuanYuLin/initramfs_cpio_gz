b7fb54137f13ce60bb89900c51be6b1b0160fd64
yuanyu.lin <yuanyu.lin@gmail.com>
2017-07-11 15:49:11 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 initramfs creator

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,35 +1,66 @@
 import ops
 import iopc
 
-def MAIN_ENV(args):
+pkg_path = ""
+output_dir = ""
+initramfs_name = "initramfs.cpio.gz"
+mkinitramfs = "mkinitramfs.sh"
+initramfs_file = ""
+
+def set_global(args):
+    global pkg_path
+    global output_dir
+    global initramfs_file
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
+    initramfs_file = ops.path_join(iopc.getOutputRootDir(), initramfs_name)
 
+def MAIN_ENV(args):
+    set_global(args)
+
+    ops.exportEnv(ops.setEnv("LINUX_INITRAMFS_NAME", initramfs_name))
     return False
 
 def MAIN_EXTRACT(args):
-    pkg_path = args["pkg_path"]
+    set_global(args)
+
+    return True
+
+def MAIN_PATCH(args, patch_group_name):
+    set_global(args)
+    for patch in iopc.get_patch_list(pkg_path, patch_group_name):
+        if iopc.apply_patch(output_dir, patch):
+            continue
+        else:
+            sys.exit(1)
 
     return True
 
 def MAIN_CONFIGURE(args):
-    output_dir = args["output_path"]
+    set_global(args)
+
     return True
 
 def MAIN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
+
+    ops.copyto(ops.path_join(pkg_path, mkinitramfs), iopc.getOutputRootDir())
+    CMD = [ops.path_join(iopc.getOutputRootDir(), mkinitramfs), ops.path_join(iopc.getOutputRootDir(), "rootfs"), initramfs_file]
+    res = ops.execCmd(CMD, iopc.getOutputRootDir(), False, None)
+    print res
+
     return False
 
 def MAIN_INSTALL(args):
-    output_dir = args["output_path"]
-    iopc.make_initramfs(iopc.getTargetRootfs(), iopc.getBaseRootFile(""))
+    set_global(args)
 
     return False
 
 def MAIN_CLEAN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN(args):
+    set_global(args)
     print "initramfs_cpio_gz"
 
